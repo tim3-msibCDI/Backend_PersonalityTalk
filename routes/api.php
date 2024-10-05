c06a326cbd87074105504fedc2d1c217873e922e
fajaryumna <fajaryumna2772@gmail.com>
2024-10-05 11:34:30 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,9 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/register-user', 'userRegisterSave')->name('register-user-save');
     Route::post('/register-mahasiswa', 'mahasiswaRegisterSave')->name('register-mahasiswa-save');
     Route::post('/login', 'userloginAction')->name('login-action');
-    Route::get('/auth/google', 'redirectToGoogle')->name('auth.google.redirect');
-    Route::get('/auth/google/callback', 'handleGoogleCallback')->name('auth.google.callback');
+    // 
+    // Route::get('/auth/google', 'redirectToGoogle')->name('auth.google.redirect');
+    // Route::get('/auth/google/callback', 'handleGoogleCallback')->name('auth.google.callback');
 });
+
+
